03470c7a07d44b54a0466676b0a311c7ffc7478a
Phani Pavan K <kphanipavan@gmail.com>
2023-04-24 20:33:43 +0530

    init changes for rock5b

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
+from periphery import GPIO
 import subprocess
 
+powerPin = GPIO(107, "high")
+powerPin.direction = 'in'
+powerPin.edge = "falling"
+powerPin.read()
+powerPin.poll()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(3, GPIO.FALLING)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.wait_for_edge(3, GPIO.FALLING)
 
 subprocess.call(['shutdown', '-h', 'now'], shell=False)
